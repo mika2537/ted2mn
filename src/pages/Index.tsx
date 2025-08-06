82a892daa047ff1b59412e6bd32f97d239ab5098
mika2537 <temitemka2537@gmail.com>
2025-08-06 11:52:03 +0800

    Create Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,15 @@
+
+import { useTranslation } from "@/components/hooks/useTranslation"; // Adjusted path
+import VideoTranslator from "@/pages/VideoTranslator";
+
+const Index = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <VideoTranslator />
+    </div>
+  );
+};
+
+export default Index;
